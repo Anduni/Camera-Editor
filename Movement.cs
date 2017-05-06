3c1d158d7c13e6210c3429600664b9b94bafce00
Anduni <anton.volkholz@me.com>
2017-05-06 18:55:13 +0200

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+
+public class Movement : MonoBehaviour {
+	
+	public float speed;
+	public float smooth;
+	float h;
+	float v;
+	float sh;
+	float sv;
+
+	void Update (){
+		h = Input.GetAxis ("Horizontal") * speed;
+		v = Input.GetAxis ("Vertical") * speed;
+		SmoothMovement ();
+	}
+
+	void FixedUpdate () {
+		transform.position = new Vector3 (transform.position.x + sh, 
+			transform.position.y, transform.position.z + sv);
+	}
+
+	void SmoothMovement() {
+		sh = Mathf.Lerp (sh, h, smooth);
+		sv = Mathf.Lerp (sv, v, smooth);
+	}
+}
